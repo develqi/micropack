a3676d523eeef84d90a070822bddd4ce946fc178
ghasemi-m <develqi@gmail.com>
2021-04-25 18:48:26 +0430

    Add IoC project

---
+++ Micropack.IoC/AutoRegisterAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Micropack.IoC
+{
+    public class AutoRegisterAttribute : Attribute
+    {
+        public ServiceLifetime ServiceLifetime { get; set; } = ServiceLifetime.Scoped;
+    }
+}
---
+++ Micropack.IoC/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Micropack.IoC
+{
+    public static class Extensions
+    {
+        public static void AddAutoRegisterServicesFromAssemblyContaining<TService>(this IServiceCollection services)
+        {
+            var assembly = typeof(TService).Assembly;
+
+            assembly.GetTypes()
+                    .Where(type => type.IsClass)
+                    .Where(type => type.GetCustomAttributes().Any(attr => attr.GetType() == typeof(AutoRegisterAttribute)))
+                    .ToList()
+                    .ForEach(type =>
+                    {
+                        var attribute = type.GetCustomAttribute(typeof(AutoRegisterAttribute));
+
+                        var serviceLifetime = (ServiceLifetime)attribute.GetType().GetProperty("ServiceLifetime").GetValue(attribute);
+
+                        services.Add(new ServiceDescriptor(type, type, serviceLifetime));
+                    });
+        }
+    }
+}
