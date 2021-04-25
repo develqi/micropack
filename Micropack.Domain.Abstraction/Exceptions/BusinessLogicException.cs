eddcb7b1201132aac8dcaed029dc8d9ee2c99dea
ghasemi-m <develqi@gmail.com>
2021-04-25 19:03:06 +0430

    Implement business logic exception

---
+++ Micropack.Domain.Abstraction/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Micropack.Domain.Abstraction
+{
+    public class BusinessLogicException : Exception
+    {
+        private readonly string _message;
+
+        public BusinessLogicException(string message) : base(message)
+        {
+            _message = message;
+        }
+
+        public override string Message => _message;
+    }
+}
