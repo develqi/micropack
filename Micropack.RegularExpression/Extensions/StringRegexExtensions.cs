048b3b51c2a98329a986412f4e76f39a857ab1a5
ghasemi-m <develqi@gmail.com>
2021-04-25 18:49:07 +0430

    Add RegularExpression project

---
+++ Micropack.RegularExpression/Extensions/StringRegexExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Micropack.RegularExpression
+{
+    public static class StringRegexExtensions
+    {
+        private static bool IsMatch(string pattern, string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return new Regex(pattern).IsMatch(input);
+        }
+
+        public static bool IsNumeric(this string input) => IsMatch(NumericPatterns.NumericPattern, input);
+        
+        public static bool IsTenDigitNumericPattern(this string input) => IsMatch(NumericPatterns.TenDigitNumericPattern, input);
+        
+        public static bool IsElevenDigitNumericPattern(this string input) => IsMatch(NumericPatterns.ElevenDigitNumericPattern, input);
+        
+        public static bool IsThirteenDigitNumericPattern(this string input) => IsMatch(NumericPatterns.ThirteenDigitNumericPattern, input);
+        
+        public static bool IsOneToTenDigitsNumericPattern(this string input) => IsMatch(NumericPatterns.OneToTenDigitsNumericPattern, input);
+        
+        public static bool IsZeroToTwentyDigitsNumericPattern(this string input) => IsMatch(NumericPatterns.ZeroToTwentyDigitsNumericPattern, input);
+        
+        public static bool IsOnlyPersianLettersAcceptNumericPattern(this string input) => IsMatch(NumericPatterns.OnlyPersianLettersAcceptNumericPattern, input);
+    }
+}
---
+++ Micropack.RegularExpression/NumericPatterns.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Micropack.RegularExpression
+{
+    public class NumericPatterns
+    {
+        public static string NumericPattern => @"^\d+$";
+
+        public static string TenDigitNumericPattern => @"^\d{10}$";
+
+        public static string ElevenDigitNumericPattern => @"^\d{11}$";
+
+        public static string ThirteenDigitNumericPattern => @"^\d{12}$";
+
+        public static string OneToTenDigitsNumericPattern => @"^\d{1,10}$";
+
+        public static string ZeroToTwentyDigitsNumericPattern => @"^\d{0,20}$";
+
+        public static string OnlyPersianLettersAcceptNumericPattern => @"^[ 0123456789۱۲۳۴۵۶۷۸۹آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$";
+    }
+}
