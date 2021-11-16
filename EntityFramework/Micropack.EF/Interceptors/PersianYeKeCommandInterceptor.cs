fa492730812726c81ef514a9bc7f5bf5991c03e7
Matin <develqi@gmail.com>
2021-11-16 17:28:20 +0330

    Move YeKe extension class into Micropack.Domain.Abstraction library

--- EntityFramework/Micropack.EF/Interceptors/PersianYeKeCommandInterceptor.cs
+++ EntityFramework/Micropack.EF/Interceptors/PersianYeKeCommandInterceptor.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Data.Common;
 using System.Globalization;
 using System.Threading.Tasks;
+using Micropack.Domain.Abstraction;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Micropack.EF.Interceptors
@@ -64,47 +65,6 @@ namespace Micropack.EF.Interceptors
                             if (parameter.Value.ToString() == "")
                                 parameter.Value = DBNull.Value;
                         }
-                        break;
-                }
-            }
-        }
-    }
-
-    public static class YeKe
-    {
-        public const char ArabicYeChar = (char)1610;
-        public const char PersianYeChar = (char)1740;
-
-        public const char ArabicKeChar = (char)1603;
-        public const char PersianKeChar = (char)1705;
-
-        public static string ApplyCorrectYeKe(this object data)
-        {
-            return data == null ? null : ApplyCorrectYeKe(data.ToString());
-        }
-
-        public static string ApplyCorrectYeKe(this string data)
-        {
-            return string.IsNullOrWhiteSpace(data) ?
-                        string.Empty :
-                        data.Replace(ArabicYeChar, PersianYeChar).Replace(ArabicKeChar, PersianKeChar).Trim();
-        }
-
-        public static void ApplyCorrectYeKe(this DbCommand command)
-        {
-            command.CommandText = command.CommandText.ApplyCorrectYeKe();
-
-            foreach (DbParameter parameter in command.Parameters)
-            {
-                switch (parameter.DbType)
-                {
-                    case DbType.AnsiString:
-                    case DbType.AnsiStringFixedLength:
-                    case DbType.String:
-                    case DbType.StringFixedLength:
-                    case DbType.Xml:
-                        if (!(parameter.Value is DBNull) && parameter.Value is string)
-                            parameter.Value = Convert.ToString(parameter.Value, CultureInfo.InvariantCulture).ApplyCorrectYeKe();
                         break;
                 }
             }
