8af04267aab3fb2dd7f3cc5c9b0f727412466230
Matin <develqi@gmail.com>
2021-11-16 17:28:43 +0330

    Handel YeKe in search oprations

---
+++ Micropack.Domain.Abstraction/Pagination/Pagination.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+namespace Micropack.Domain.Abstraction
+{
+    public record Pagination<TResponse>(int Page, int TotalRecords, IEnumerable<TResponse> Records) where TResponse : class, new();
+}
---
+++ Micropack.Domain.Abstraction/Pagination/PaginationRequest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Micropack.Domain.Abstraction
+{
+    public record PaginationRequest
+    {
+        private int _page = 1;
+        private int _size = 10;
+
+        public int Page
+        {
+            get => _page;
+            set
+            {
+                if(value < 1)
+                    throw new ArgumentException("Page parameter greater than 0.");
+
+                _page = value;
+            }
+        }
+
+        public int Size
+        {
+            get => _size;
+            set
+            {
+                if (value < 1 || value > 100)
+                    throw new ArgumentException("Size parameter must between 1 to 100.");
+
+                _size = value;
+            }
+        }
+
+        private string _filter;
+
+        public string Filter
+        {
+            get => _filter.ApplyCorrectYeKe();
+            set => _filter = value;
+        }
+
+        public string OrderBy { get; set; }
+
+        public string Select { get; set; }
+
+        public int RecordId { get; set; }
+
+        public bool NoSort => string.IsNullOrWhiteSpace(OrderBy);
+
+        public bool NoFilter => string.IsNullOrWhiteSpace(Filter);
+
+        public bool NoSelect => string.IsNullOrWhiteSpace(Select);
+
+        //public string QueryString => GetQueryString();
+
+        //private string GetQueryString()
+        //{
+        //    var queryString = new StringBuilder($"?page={Page}&size={Size}");
+
+        //    queryString.Append(NoSort ? string.Empty : $"&orderby={OrderBy}");
+        //    queryString.Append(NoSelect ? string.Empty : $"&select={Select}");
+        //    queryString.Append(NoFilter ? string.Empty : $"&filter={Filter}");
+
+        //    return queryString.ToString();
+        //}
+    }
+}
